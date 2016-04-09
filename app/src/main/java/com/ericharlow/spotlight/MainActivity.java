c9a2acd2a26f1e4707e57090eac254e72bf90792
Eric Harlow <eric.b.harlow@gmail.com>
2016-04-08 19:13:17 -0600

    added library module and boilerplate app ui

--- app/src/main/java/com/ericharlow/spotlight/MainActivity.java
+++ app/src/main/java/com/ericharlow/spotlight/MainActivity.java
@@ -1,13 +1,35 @@
 package com.ericharlow.spotlight;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onFirstButtonClicked(View v) {
+        v.setVisibility(View.GONE);
+    }
+
+    public void onSecondButtonClicked(View v) {
+        v.setVisibility(View.GONE);
+    }
+
+    public void onThirdButtonClicked(View v) {
+        v.setVisibility(View.GONE);
+    }
+
+    public void onFourthButtonClicked(View v) {
+        v.setVisibility(View.GONE);
+    }
+
+    public void onNewActivityClicked(View v) {
+        v.setVisibility(View.GONE);
     }
 }
